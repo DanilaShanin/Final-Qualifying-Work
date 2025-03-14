6c54b5f8ecd6b0061089542ab91fe69985d9c09b
DanilaShanin <Danilashanin2002@gmail.com>
2025-03-14 15:28:06 +0300

    creating add window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class Main(tk.Frame):
@@ -15,8 +16,36 @@ class Main(tk.Frame):
                                     compound=tk.TOP, image=self.add_img)
         btn_open_dialog.pack(side=tk.LEFT)
 
+        self.tree = ttk.Treeview(self, columns=(
+        'ID', 'Make', 'Name', 'Transmission', 'EngineType', 'EngineCapacity', 'Mileage', 'City', 'Year', 'Price')
+                                 , height=15, show='headings')
+        self.tree.column("ID", width=10, anchor=tk.CENTER)
+        self.tree.column("Make", width=150, anchor=tk.CENTER)
+        self.tree.column("Name", width=55, anchor=tk.CENTER)
+        self.tree.column("Transmission", width=55, anchor=tk.CENTER)
+        self.tree.column("EngineType", width=150, anchor=tk.CENTER)
+        self.tree.column("EngineCapacity", width=400, anchor=tk.CENTER)
+        self.tree.column("Mileage", width=100, anchor=tk.CENTER)
+        self.tree.column("City", width=150, anchor=tk.CENTER)
+        self.tree.column("Year", width=100, anchor=tk.CENTER)
+        self.tree.column("Price", width=100, anchor=tk.CENTER)
+
+        self.tree.heading("ID", text='ID')
+        self.tree.heading("Make", text='Марка')
+        self.tree.heading("Name", text='Наименование')
+        self.tree.heading("Transmission", text='Коробка передач')
+        self.tree.heading("EngineType", text='Тип двигателя')
+        self.tree.heading("EngineCapacity", text='Мощьность двигателя')
+        self.tree.heading("Mileage", text='Пробег')
+        self.tree.heading("Year", text='Год выпуска')
+        self.tree.heading("Price", text='Цена')
+
+
+        self.tree.pack(side=tk.LEFT)
+
     def open_dialog(self):
         Child()
+
 
 class Child(tk.Toplevel):
     def __init__(self):
@@ -25,11 +54,61 @@ class Child(tk.Toplevel):
 
     def init_child(self):
         self.title('Добавить')
-        self.geometry('400x220+400+300')
-        self.resizable(False, False)
+        self.geometry('1000x1000+400+300')
 
-        self.grab_set()
-        self.focus_set()
+        label_Make = tk.Label(self, text='Имя:')
+        label_Make.place(x=100, y=25)
+        label_Name = tk.Label(self, text='Возраст:')
+        label_Name.place(x=100, y=50)
+        label_EngineType = tk.Label(self, text='Позиция')
+        label_EngineType.place(x=100, y=100)
+        label_EngineCapacity = tk.Label(self, text='Информация:')
+        label_EngineCapacity.place(x=100, y=125)
+        label_Mileage = tk.Label(self, text='Гражданство:')
+        label_Mileage.place(x=100, y=150)
+        label_City = tk.Label(self, text='Клуб:')
+        label_City.place(x=100, y=175)
+        label_Year = tk.Label(self, text='Цена:')
+        label_Year.place(x=100, y=200)
+        label_Price = tk.Label(self, text='Цена:')
+        label_Price.place(x=100, y=200)
+
+        self.entry_Make = ttk.Entry(self)
+        self.entry_Make.place(x=200, y=25)
+
+        self.entry_Name = ttk.Entry(self)
+        self.entry_Name.place(x=200, y=50)
+
+        self.entry_Name = ttk.Entry(self)
+        self.entry_Name.place(x=200, y=50)
+
+        self.entry_Name = ttk.Entry(self)
+        self.entry_Name.place(x=200, y=50)
+
+        self.transmission = ttk.Entry(self)
+        self.transmission.place(x=200, y=75)
+
+        self.EngineType = ttk.Entry(self)
+        self.EngineType.place(x=200, y=100)
+
+        self.entry_EngineCapacity = ttk.Entry(self)
+        self.entry_EngineCapacity.place(x=200, y=125)
+
+        self.entry_Mileage = ttk.Entry(self)
+        self.entry_Mileage.place(x=200, y=150)
+
+        self.entry_City = ttk.Entry(self)
+        self.entry_City.place(x=200, y=175)
+
+        self.entry_Year = ttk.Entry(self)
+        self.entry_Year.place(x=200, y=200)
+
+        self.entry_Price = ttk.Entry(self)
+        self.entry_Price.place(x=200, y=200)
+
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        btn_cancel.place(x=300, y=500)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -39,9 +118,3 @@ if __name__ == "__main__":
     root.geometry("650x450+300+200")
     root.resizable(False, False)
     root.mainloop()
-
-
-
-
-
-
