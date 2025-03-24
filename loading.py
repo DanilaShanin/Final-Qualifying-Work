56d701664668d7f605216fa606288d8a59e06d27
DanilaShanin <Danilashanin2002@gmail.com>
2025-03-24 08:44:40 +0300

    In Loading add login

--- loading.py
+++ loading.py
@@ -1,39 +1,66 @@
 
 import time
-
+from tkinter import tk
 import PySide6
-
-from main import Main, main
-from random import randint
-import NonExistentModule
 from PySide6.QtWidgets import QApplication, QSplashScreen, QProgressBar
 from PySide6.QtGui import QMovie, QPixmap, QPainter
 from PySide6.QtCore import QSize
-
 from main import Main, main
 from random import randint
 
 class MovieSplashScreen(QSplashScreen):
     my_size = QSize(600, 600)
 
-    def __init__(path_to_gif: str)
+    def __init__(self, path_to_gif: str):
         self.movie = QMovie(path_to_gif)
         self.movie.jumpToFrame(0)
         pixmap = QPixmap(self.my_size)
         QSplashScreen.__init__(self, pixmap)
         self.movie.frameChanged.connect(self.repaint)
 
-    def showEvent(event: PySide6.QtGui.QShowEvent) -> None:
-        while True:
-            self.movie.start()
+    def showEvent(self, event:PySide6.QtGui.QShowEvent) -> None:
+        self.movie.start()
 
-    def hideEvent(event: PySide6.QtGui.QHideEvent) -> None:
+    def hideEvent(self, event:PySide6.QtGui.QHideEvent) -> None:
         self.movie.stop()
 
-    def paintEvent(event: PySide6.QtGui.QPaintEvent) -> None:
+    def paintEvent(self, event:PySide6.QtGui.QPaintEvent) -> None:
         painter = QPainter(self)
         pixmap = self.movie.currentPixmap()
         pixmap = pixmap.scaled(self.my_size)
         painter.drawPixmap(0, 0, pixmap)
 
+def login():
+    app = QApplication()
+    progressbar_value = 30
+    path_to_gif = f'loading_gifs/{rnd.randint(1, 5)}.gif'
 
+    splash = MovieSplashScreen(path_to_gif)
+    progressbar = QProgressBar(splash)
+    progressbar.setMaximum(progressbar_value)
+    progressbar.setTextVisible(False)
+    progressbar.setGeometry(0, splash.my_size.height() - 50,  # Ошибка: my_size не существует
+                            splash.my_size.width(), 20)       # Ошибка: my_size не существует
+
+    splash.show()
+
+    for i in range(progressbar_value):
+        progressbar.setValue(i)
+        t = time.time()
+        while time.time() < t + 0.1:
+            app.processEvents()
+
+    time.sleep(1)
+    main()
+    root = Tk()
+    # db = DB()  # Ошибка: DB не определен
+    app = Main(root)  # Перезапись переменной app
+    app.pack()
+    root.title("FootScaut")
+    root.geometry("1400x900+300+200")
+    root.resizable(True, True)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    login()
