90a225a661d70f3d874ba4ede30e233f9d187541
DanilaShanin <Danilashanin2002@gmail.com>
2025-03-28 17:48:44 +0300

    update tk_loader

--- tk_loader.py
+++ tk_loader.py
@@ -1,6 +1,11 @@
 from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from time import sleep
+import multiprocessing as mp
+from multiprocessing import Process
 from random import randint
-import tkinter as ttk
+
 
 class Screensaver:
     _image = None
@@ -27,7 +32,6 @@ class Screensaver:
 
         self._root.after(0, lambda: self._window_update_thread(frames, frame_amount, delay))
 
-
     def _open(self):
         self._image_data = Image.open(self._image)
 
@@ -50,15 +54,55 @@ class Screensaver:
             shared_list.append(self._image_data)
             # print('Thread:', i)
 
+    def _window_update_thread(self, shated_list, frame_amount, delay):
+        sleep(1)  # Ждем загрузки нескольких кадров
 
+        i = 0
+        while self._working:
+            image = ImageTk.PhotoImage(shated_list[
+                                           i])  # Конвертируем изображения из формата библиотеки Pillow в формат, который может понять Tkinter
+            # Не сохраняем изображения в данном формате изначально, потому, что его нельзя серилизовать библиотекой pickle
+            # библиотека multiprocessing, общие данные под капотом серилизует библиотекой pickle
+            self._canvas.create_image(0, 0, anchor='nw', image=image)
+            i += 1
+
+            if self._process_func:
+                self._process_func(1 / frame_amount * 100)
+
+            # print('GUI:',i)
+            if i >= frame_amount:
+                i = 0
+
+                if self._go_next:
+                    self._working = False
+                    self._go_next()
+
+            self._canvas.update()
+            sleep(delay)
+
+    def get_frame_amount(self):
+        return self._image_data.n_frames
+
+    def get_image_size(self):
+        return (self._image_data.height, self._image_data.width)
+
+
+def update_progress(value):
+    bar.step(value)
+
+
+def navigate_to_app():
+    update_progress(100)
 
 
 if __name__ == "__main__":
     root = Tk()
+    load_screen = Screensaver(root, progress_func=update_progress, go_next=navigate_to_app)
     bar = ttk.Progressbar(orient='horizontal')
     bar.pack(fill='both')
 
     image_id = randint(1, 4)
+    load_screen.play_gif(f'loading_gifs/{image_id}.gif', 0.03)
 
     root.title("FootScaut")
     root.mainloop()
