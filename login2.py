9c3f8570518d2ac4c3d27cf5ca7d513734035ef2
DanilaShanin <Danilashanin2002@gmail.com>
2025-06-16 18:44:15 +0300

    Update login2.py

--- login2.py
+++ login2.py
@@ -2,7 +2,12 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 import hashlib
+# from Admin import Adminapp
+import Admin
+global app
+import hashlib
 from loading import login
+from main import Main
 
 class MainWindow(tk.Tk):
     def __init__(self):
@@ -33,7 +38,8 @@ class MainWindow(tk.Tk):
     def check_credentials(self, username, password):
         conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, self.get_hashed_password(password)))
+        c.execute("SELECT * FROM users WHERE username = ? AND password = ?",
+                  (username, self.get_hashed_password(password)))
         result = c.fetchone()
         conn.close()
         return bool(result)
@@ -42,7 +48,8 @@ class MainWindow(tk.Tk):
         conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
         try:
-            c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, self.get_hashed_password(password)))
+            c.execute("INSERT INTO users (username, password) VALUES (?, ?)",
+                      (username, self.get_hashed_password(password)))
             conn.commit()
             return True
         except sqlite3.IntegrityError:
@@ -60,8 +67,8 @@ class MainWindow(tk.Tk):
         username_entry = tk.Entry(self)
         password_label = tk.Label(self, text="Пароль:")
         password_entry = tk.Entry(self, show="*")  # Скрывает вводимый пароль
-
-        login_button = tk.Button(self, text="Войти", command=lambda: self.loginpas(username_entry.get(), password_entry.get()))
+        login_button = tk.Button(self, text="Войти",
+                                 command=lambda: self.loginpas(username_entry.get(), password_entry.get()))
         register_button = tk.Button(self, text="Зарегистрироваться", command=self.register_user)
 
         # Размещение виджетов на экране
@@ -72,17 +79,38 @@ class MainWindow(tk.Tk):
         login_button.pack(pady=10)
         register_button.pack(pady=5)
 
+
+
     def loginpas(self, username, password):
+        is_Admin = True
+        admin_name = 'admin'
+        for i, j in zip(username.lower(),admin_name):
+            if i !=j:
+                is_Admin = False
+                break
+        if is_Admin:
+            username = 'admin'
         if self.check_credentials(username, password):
             messagebox.showinfo("Успех", "Вы успешно вошли!")
-            self.destroy()  # Закрытие окна входа
-            self.open_main_app()  # Открытие главного приложения
-            login()
+
+            # Проверка на логин 'admin' и соответствующий пароль
+            if username == 'admin':
+                self.destroy()  # Закрываем текущее окно
+                root = tk.Tk()  # Создаем новое корневое окно Tkinter
+                app = Admin.Adminapp(root)  # Инициализируем приложение администратора
+                app.pack()
+                root.title("Администраторское приложение")
+                root.geometry("800x600")
+                root.mainloop()  # Запускаем цикл обработки событий Tkinter
+            else:
+                self.destroy()  # Закрытие окна входа
+                self.open_main_app()  # Открытие главного приложения
+                login()
         else:
             messagebox.showerror("Ошибка", "Неправильное имя пользователя или пароль.")
 
     def open_main_app(self):
-        pass  # Здесь будет реализована логика вашего приложения
+        pass
 
 
 class RegisterWindow(tk.Toplevel):
@@ -94,21 +122,20 @@ class RegisterWindow(tk.Toplevel):
         self.create_register_form()
 
     def create_register_form(self):
-        # Создание виджетов для формы регистрации
+
         username_label = tk.Label(self, text="Имя пользователя:")
         username_entry = tk.Entry(self)
         password_label = tk.Label(self, text="Пароль:")
         password_entry = tk.Entry(self, show="*")  # Скрывает вводимый пароль
         confirm_password_label = tk.Label(self, text="Подтверждение пароля:")
-        confirm_password_entry = tk.Entry(self, show="*")  # Скрывает вводимый пароль
+        confirm_password_entry = tk.Entry(self, show="*")
 
         register_button = tk.Button(self, text="Зарегистрироваться", command=lambda: self.register(
             username_entry.get(),
             password_entry.get(),
             confirm_password_entry.get())
-        )
+                                    )
 
-        # Размещение виджетов на экране
         username_label.pack(pady=5)
         username_entry.pack(pady=5)
         password_label.pack(pady=5)
